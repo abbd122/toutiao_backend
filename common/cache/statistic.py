e8522487f30d09dad139d2da5ee7d0829f07898f
abbd122 <446404815@qq.com>
2019-07-17 15:23:31 +0800

    添加定时器任务，定期修复用户关注及用户文章数据

--- common/cache/statistic.py
+++ common/cache/statistic.py
@@ -1,5 +1,10 @@
 from flask import current_app
 from redis.exceptions import RedisError
+from sqlalchemy import func
+from models import db
+
+from common.models.news import Article
+from common.models.user import Relation
 
 
 class CountStorageBase(object):
@@ -44,14 +49,57 @@ class CountStorageBase(object):
 
         current_app.redis_master.zincrby(cls.key, user_id, increment)
 
+    @classmethod
+    def reset(cls, db_query_ret):
+        '''
+        定时修复缓存数据
+        :param db_query_ret: (date_id, count) Mysql数据库查询出的最新id和对应的统计数量
+        :return: 无
+        '''
+
+        # 创建Redis管道对象
+        pl = current_app.redis_master.pipeline()
+        # 删除Redis中的用户缓存记录
+        pl.delete(cls.key)
+        redis_data = []
+        # 获取Mysql中查询出的最新数据
+        for date_id, count in db_query_ret:
+            # 追加到列表中
+            redis_data.append(count)
+            redis_data.append(date_id)
+        # redis_data = [count1, date_id_1, count2, date_id_2, ...]
+        # 调用管道对象储存最新数据到Redis
+        pl.zadd(cls.key, *redis_data)
+        # 执行管道对象
+        pl.execute()
+
 
 class UserArticleCountStorage(CountStorageBase):
     '''用户文章统计'''
 
     key = 'count:user:arts'
 
+    # 静态方法
+    @staticmethod
+    def db_query():
+        '''
+        查询数据库中最新文章数据
+        :return: 最新文章统计数据和对应的user_id
+        '''
+        # 查询出所有最新审核通过的文章数量及其所对应的user_id,按用户进行分组
+        result = db.session.query(Article.user_id, func.count(Article.id))\
+            .filter(Article.status == Article.STATUS.APPROVED).group_by(Article.user_id).all()
+        return result
+
 
 class UserFollowingCountStorage(CountStorageBase):
     '''用户被关注的数量'''
 
     key = 'count:user:followings'
+
+    @staticmethod
+    def db_query():
+        # 查询出最新的用户粉丝数
+        result = db.session.query(Relation.user_id, func.count(Relation.target_user_id))\
+            .filter(Relation.relation == Relation.RELATION.FOLLOW).group_by(Relation.user_id).all()
+        return result
---
+++ toutiao/schedule/statistics.py
@@ -0,0 +1,28 @@
+from common.cache.statistic import *
+
+
+def fix_thread(count_storage_cls):
+    '''
+    修复数据工具方法
+    :param count_storage_cls: 统计类
+    :return: 无
+    '''
+
+    # 查询数据库中最新数据
+    result = count_storage_cls.db_query()
+    # 重设Redis缓存数据
+    count_storage_cls.reset(result)
+
+
+def fix_statistic(flask_app):
+    '''
+    修正统计数据
+    :return:
+    '''
+
+    # 开启flask上下文
+    with flask_app.app_context():
+        # 调用修复工具修复用户文章统计数据
+        fix_thread(UserArticleCountStorage)
+        # 调用修复工具修复用户被关注的数量
+        fix_thread(UserFollowingCountStorage)
